4f05b253bc8489800d926de3616d74e8c9c0fdf0
Zhang Chao <Frodocz@users.noreply.github.com>
2022-11-11 09:28:55 +0000

    implement scanner

--- include/scanner.h
+++ include/scanner.h
@@ -2,6 +2,7 @@
 #define __ZSON_SCANNER_H__
 
 #include <string>
+#include <cstddef>
 
 namespace zson {
 
@@ -24,12 +25,29 @@ public:
         BEGIN_ARRAY,        // [
         END_ARRAY,          // ]
 
-        END_OF_SOURCE       // EOF
+        END_OF_SOURCE,      // EOF
+
+        VALUE_ERROR,        // Unsupport Token
     };
 
     JsonTokenType scan();
 private:
-    std::string m_source;   // json source string
+    bool isAtEnd() const { return m_curIdx >= m_source.size(); }
+    char advance() { return m_source[m_curIdx++]; }
+    char peek() { return isAtEnd() ? '\0' : m_source[m_curIdx]; }
+    char peekNext() { return ((m_curIdx + 1) >= m_source.size()) ? '\0' : m_source[m_curIdx + 1]; }
+
+    void scanTrue();
+    void scanFalse();
+    void scanNull();
+    void scanString();
+    void scanNumber();
+
+private:
+    std::string m_source;       // JSON source string
+    size_t m_curIdx;            // current index when scanning the JSON string
+    std::string m_val_string;   // store string value
+    double m_val_number;        // store number value
 };
 
 } // namespace zson
---
+++ src/scanner.cpp
@@ -0,0 +1,102 @@
+#include <stdexcept>
+#include "include/scanner.h"
+
+namespace zson {
+
+void Scanner::scanTrue() {
+    if (m_source.compare(m_curIdx, 3, "rue") == 0) {
+        m_curIdx += 3;
+    } else {
+        throw std::logic_error("Scan 'true' failed");
+    }
+}
+
+void Scanner::scanFalse() {
+    if (m_source.compare(m_curIdx, 4, "alse") == 0) {
+        m_curIdx += 4;
+    } else {
+        throw std::logic_error("Scan 'false' failed");
+    }
+}
+
+void Scanner::scanNull() {
+    if (m_source.compare(m_curIdx, 3, "ull") == 0) {
+        m_curIdx += 3;
+    } else {
+        throw std::logic_error("Scan 'null' failed");
+    }
+}
+
+void Scanner::scanString() {
+    size_t pos = m_curIdx;
+    while (peek() != '\"' && !isAtEnd()) {
+        advance();
+    }
+
+    if (isAtEnd()) {
+        throw std::logic_error("Invalid string: missing closing quote!");
+    }
+
+    advance();
+    m_val_string = m_source.substr(pos, m_curIdx - pos - 1);
+}
+
+void Scanner::scanNumber() {
+    size_t pos = m_curIdx - 1;
+    while (isdigit(peek())) {
+        advance();
+    }
+
+    if (peek() == '.' && isdigit(peekNext())) {
+        advance();
+        while (isdigit(peek())) {
+            advance();
+        }
+    }
+
+    m_val_number = std::atof(m_source.substr(pos, m_curIdx - pos).c_str());
+}
+
+Scanner::JsonTokenType Scanner::scan() {
+    if (isAtEnd()) {
+        return JsonTokenType::END_OF_SOURCE;
+    }
+
+    char c = advance();
+    switch (c) {
+        case '{':
+            return JsonTokenType::BEGIN_OBJECT;
+        case '}':
+            return JsonTokenType::END_OBJECT;
+        case '[':
+            return JsonTokenType::BEGIN_ARRAY;
+        case ']':
+            return JsonTokenType::END_ARRAY;
+        case ':':
+            return JsonTokenType::NAME_SEPARATOR;
+        case ',':
+            return JsonTokenType::VALUE_SEPARATOR;
+        case 't':
+            return JsonTokenType::LITERAL_TRUE;
+        case 'f':
+            return JsonTokenType::LITERAL_FALSE;
+        case 'n':
+            return JsonTokenType::LITERAL_NULL;
+        case ' ':
+        case '\n':
+        case '\t':
+        case '\r':
+            return scan();
+        case '\"':
+            scanString();
+            return JsonTokenType::VALUE_STRING;
+        default:
+            if (isdigit(c) || c == '-') {
+                scanNumber();
+                return JsonTokenType::VALUE_NUMBER;
+            }
+    }
+    return JsonTokenType::VALUE_ERROR;
+}
+
+} // namespace zson
